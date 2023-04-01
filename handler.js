061b6f06f2049a608391d12a4f8e9c96db297289
timmoth <t.jones@timmoth.com>
2023-04-01 20:00:16 +0100

    added influx db

--- handler.js
+++ handler.js
@@ -1,12 +1,37 @@
 'use strict';
+//import InfluxDB client, this is possible thanks to the layer we created
+const { InfluxDB, Point, } = require('@influxdata/influxdb-client')
+//grab environment variables
+const org = process.env.org
+const bucket = process.env.bucket
+const token = process.env.token;
+const url = process.env.url
 
-module.exports.helloWorld = (event, context, callback) => {
+module.exports.helloWorld = async (event, context, callback) => {
+
+  //parse the expected JSON from the body of the POST request
+  var body = JSON.parse(event.body)
+
+  //create InfluxDB api client with URL and token, then create Write API for the specific org and bucket
+  const writeApi = await new InfluxDB({ url, token }).getWriteApi(org, bucket);
+
+  //create a data point with health as the measurement name, a field value for heart beat, and userID tag
+  const dataPoint = new Point('health')
+    .tag('userID', body['userID'])
+    .floatField('heartRate', body['heartbeatRate'])
+
+  //write data point
+  await writeApi.writePoint(dataPoint)
+
+  //close write API
+  await writeApi.close().then(() => {
+    console.log('WRITE FINISHED')
+  })
+
+  //send back response to the client
   const response = {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: 'Hello, world!',
+    body: JSON.stringify('Write successful'),
   };
 
   callback(null, response);
